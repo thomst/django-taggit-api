bf592e099779a49cf40556f8cc75d23902066f90
Thomas Leichtfuß <leichtfuss@systopia.de>
2024-11-13 16:19:21 +0100

    skip support for Django <3.2

--- setup.py
+++ setup.py
@@ -51,8 +51,6 @@ setup(
     classifiers=[
         dev_status,
         "Framework :: Django",
-        "Framework :: Django :: 3.0",
-        "Framework :: Django :: 3.1",
         "Framework :: Django :: 3.2",
         "Framework :: Django :: 4.0",
         "Framework :: Django :: 4.1",
